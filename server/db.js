cc24183e02e015f247d5e53110a1af8280cb82bf
fr11nik <fruinik@gmail.com>
2023-06-20 04:49:35 +0300

    bruh

--- server/db.js
+++ server/db.js
@@ -1,12 +1,13 @@
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
+
 module.exports = new Sequelize(
   process.env.DB_NAME, // Название БД
   process.env.DB_USER, // Пользователь
   process.env.DB_PASSWORD, // ПАРОЛЬ
   {
-    //config to connect to the db
-    dialect: "postgres",
+    // Конфигурация для подключения к MySQL
+    dialect: "mysql",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     logging: (sql) => {
